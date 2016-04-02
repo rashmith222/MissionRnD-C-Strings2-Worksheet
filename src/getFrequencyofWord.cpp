c252a7fd7a137a5aade4383f5533bb07f0d96dbf
rashmith222 <rashmith222@missionrnd.com>
2016-04-02 18:05:06 +0530

    Submit Commit

--- src/getFrequencyofWord.cpp
+++ src/getFrequencyofWord.cpp
@@ -13,27 +13,31 @@ Note: Dont modify original str or word,Just return count ,Spaces can also be par
 #include <stdlib.h>
 
 int count_word_in_str_way_1(char *str, char *word){
-	int i = 0, j = 0,flag=0, length = 0, length1 = 0, count = 0;
-	while (str[length] != '\0'){
-		length++;
-	}
-	while (word[length1] != '\0'){
-		length1++;
-	}
-	while (i < length){
-		j = 0;
-		while (word[j]==str[i]){
-			j++;
-			i++;
-			flag = 1;
-			if (j == length1)
-				break;
+		int k = 0, i = 0, j = 0, flag = 0, length = 0, length1 = 0, count = 0, ctr = 0;
+		while (str[length] != '\0'){
+			length++;
 		}
-		if (flag==1)
-		count++;
-		i++;
-	}
+		while (word[length1] != '\0'){
+			length1++;
+		}
+		for (i = 0; i < length; i++)
+		{
+			k = i;
+			flag = 0;
+			ctr = 0;
+			for (j = 0; j < length1; j++)
+			{
+				if (word[j] == str[k])
+				{
+					k++;
+					ctr++;
+				}
+				if (length1 == ctr)
+					flag = 1;
+			}
+			if (flag == 1)
+				count++;
+		}
 	return count;
+
 }
-
-
--- src/getLastWord.cpp
+++ src/getLastWord.cpp
@@ -11,7 +11,7 @@ Note:Dont modify original string Neglect Spaces at the right end and at left end
 #include <stdlib.h>
 #include<malloc.h>
 char * get_last_word(char * str){
-	int length = 0, j = 0, i = 0, count = 0, count1 = 0, count2 = 0, length1 = 0;
+	int length = 0, j = 0, i = 0, count = 0, count1 = 0, count2 = 0;
 	while (str[i] != '\0'){
 		length++;
 		i++;
